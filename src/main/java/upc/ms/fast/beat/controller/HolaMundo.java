f5a9cafbef066e064ecc7727476de1f90b39c8ee
paullugo <paul@preventor.com>
2022-05-02 23:30:13 -0500

    update

--- src/main/java/upc/ms/fast/beat/controller/HolaMundo.java
+++ src/main/java/upc/ms/fast/beat/controller/HolaMundo.java
@@ -1,15 +1,23 @@
 package upc.ms.fast.beat.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import upc.ms.fast.beat.utils.WhatsAppUtil;
 
 @Controller
 @RequestMapping("/home")
 public class HolaMundo {
 
+
+    @Autowired
+    private WhatsAppUtil whatsAppUtil;
+
+
     @GetMapping("/message")
     public String home(){
+        whatsAppUtil.sendWhatsApp("+51934758933", "test");
         return "Hola";
     }
 
